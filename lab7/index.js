d8987ff9693aa46dccbf5570e903f8f679521ad7
Awaab Elamin <AwaabElamin@gmail.com>
2021-06-01 13:53:22 -0500

    lab7

---
+++ lab7/index.js
@@ -0,0 +1,37 @@
+let firstName, lastName, pic, email, phone;
+window.onload = function () {
+    refresh();
+    document.getElementById("refresh").onclick = refresh;
+
+}
+function refresh() {
+    getUser("https://randomuser.me/api/").then(() => {
+        document.getElementById("name").innerHTML = firstName + " " + lastName
+        document.getElementById("image").src = pic;
+        document.getElementById("phone").innerHTML = phone;
+        document.getElementById("email").innerHTML = email;
+    });
+    getUser("https://randomuser.me/api/").then(() => {
+        document.getElementById("name2").innerHTML = firstName + " " + lastName
+        document.getElementById("image2").src = pic;
+        document.getElementById("phone2").innerHTML = phone;
+        document.getElementById("email2").innerHTML = email;
+    });
+    getUser("https://randomuser.me/api/").then(() => {
+        document.getElementById("name3").innerHTML = firstName + " " + lastName
+        document.getElementById("image3").src = pic;
+        document.getElementById("phone3").innerHTML = phone;
+        document.getElementById("email3").innerHTML = email;
+    });
+}
+async function getUser(url) {
+    const res = await fetch(url);
+    const obj = await res.json();
+    firstName = obj.results[0].name.first;
+    lastName = obj.results[0].name.last;
+    pic = obj.results[0].picture.large;
+    email = obj.results[0].email;
+    phone = obj.results[0].phone;
+
+
+}
